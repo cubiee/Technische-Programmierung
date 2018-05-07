b85123788f68ba0be09cf73197041e04551cb845
Joshua Harig <j.harig95@hotmail.de>
2018-05-07 13:56:55 +0200

    update analyse.c

--- Uebung08/analyse.c
+++ Uebung08/analyse.c
@@ -2,7 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NAMEBUFFER_SIZE 30
+#define LINEBUFFER_SIZE 1000
+#define UPPERCASE "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define LOWERCASE "abcdefghijklmnopqrstuvwxyz"
+#define SPECIAL_CHAR "!\"'$%&'()*+,./:;<=>?@[\\]^_`{|}~0123456789"
+
+typedef struct fileinfo_s{
+	char name[NAMEBUFFER_SIZE];
+	int uppercase;
+	int lowercase;
+	int special_char;
+	int rows;
+}FILEINFO;
+
 void menue(void);
+
+FILEINFO new_file_info(void);
+FILE *open_new_file(char *filename, char *mode, FILEINFO *fileinfo_buffer);
+void analyse_file(FILE* datei, FILEINFO *fileinfo_buffer);
+void print_file_info(FILEINFO fileinfo);
 
 
 int main(int argc, char *argv[]){
@@ -11,50 +30,14 @@ int main(int argc, char *argv[]){
 	}
 	else{
 		FILE *datei;
-		if (NULL == (datei = fopen(argv[1], "r"))){
-			printf("Konnte datei nicht öffnen\n");
+		FILEINFO fileinfo = new_file_info();
+
+		if (NULL == (datei = open_new_file(argv[1], "r", &fileinfo))){
 			menue();
 		}
 		else{
-			fseek(datei, 0, SEEK_END);
-			int anzahl_zeichen = ftell(datei);
-			rewind(datei);
-			char character;
-			int grossbuchstaben = 0;
-			int kleinbuchstaben = 0;
-			int sonderzeichen = 0;
-			int sonstige_zeichen = 0;
-			int zeilenanzahl = 1;
-
-			for (int i = 0; i <= anzahl_zeichen; i++){
-				character = fgetc(datei);
-				if (NULL != strchr("ABCDEFGHIJKLMNOPQRSTUVWXYZ", character)){
-					grossbuchstaben++;
-				}
-				else if (NULL != strchr("abcdefghijklmnopqrstuvwxyz", character)){
-					kleinbuchstaben++;
-				}
-				else if (NULL != strchr("!\"'$%&'()*+,./:;<=>?@[\\]^_`{|}~0123456789", character)){
-					sonderzeichen++;
-				}
-				else{
-					sonstige_zeichen++;
-					if (character == '\n'){
-						zeilenanzahl++;
-					}
-				}
-			}
-			printf("%s\n", argv[1]);
-			printf("Zeilenanzahl: %d\n", zeilenanzahl);
-			printf("Grossbuchstaben: %d\n", grossbuchstaben);
-			printf("Kleinbuchstaben: %d\n", kleinbuchstaben);
-			printf("Sonderzeichen: %d\n", sonderzeichen);
-			printf("Sonstige Zeichen: %d\n", sonstige_zeichen);
+			print_file_info(fileinfo);
 		}
-		
-
-
-
 	}
 	return 0;
 }
@@ -62,4 +45,55 @@ int main(int argc, char *argv[]){
 void menue(void){
 	printf("Aufruf durch:\n");
 	printf("\t./analyse <dateiname>\n");
+}
+
+FILEINFO new_file_info(void){
+	FILEINFO new_fileinfo;
+	new_fileinfo.name[0] = '\0';
+	new_fileinfo.lowercase = 0;
+	new_fileinfo.uppercase = 0;
+	new_fileinfo.special_char = 0;
+	new_fileinfo.rows = 0;
+	return new_fileinfo;
+}
+
+void print_file_info(FILEINFO fileinfo){
+	printf("Name:            %s\n", fileinfo.name);
+	printf("Zeilenanzahl:    %i\n", fileinfo.rows);
+	printf("Grossbuchstaben: %i\n", fileinfo.uppercase);
+	printf("Kleinbuchstaben: %i\n", fileinfo.lowercase);
+	printf("Sonderzeichen:   %i\n", fileinfo.special_char);
+}
+
+FILE *open_new_file(char *filename, char *mode, FILEINFO *fileinfo_buffer){
+	FILE *datei;
+	if (NULL == (datei = fopen(filename, mode))){
+		perror("FEHLER");
+		return NULL;
+	}
+	else{
+		strncpy(fileinfo_buffer->name, filename, NAMEBUFFER_SIZE);
+		analyse_file(datei, fileinfo_buffer);
+		return datei;
+	}
+}
+
+void analyse_file(FILE *datei, FILEINFO *fileinfo_buffer){
+	char linebuffer[LINEBUFFER_SIZE] = { '\0' };
+	while (NULL != fgets(linebuffer, LINEBUFFER_SIZE, datei)){
+		fileinfo_buffer->rows += 1;
+		for (int i = 0; i < strlen(linebuffer); i++){
+			if (NULL != strchr(UPPERCASE, linebuffer[i])){
+				fileinfo_buffer->uppercase += 1;
+			}
+			else if (NULL != strchr(LOWERCASE, linebuffer[i])){
+				fileinfo_buffer->lowercase += 1;
+			}
+			else if (NULL != strchr(SPECIAL_CHAR, linebuffer[i])){
+				fileinfo_buffer->special_char += 1;
+			}
+		}
+	}
+	rewind(datei);
+	return;
 }
