2f11e2d61274705400d116b058edd91cba4d9092
Joshua Harig <j.harig95@hotmail.de>
2018-05-14 21:43:06 +0200

    kommentare hinzugefuegt

--- Uebung08/geheim.c
+++ Uebung08/geheim.c
@@ -6,6 +6,8 @@
 #define FALSE 0
 #define MAX_NAME_SIZE 31
 
+
+//Struktur fuer Eingangsparameter + funktion zum erstellen mit defaultwerten
 typedef struct input_parameter_s{
 	char input_file[MAX_NAME_SIZE ];
 	char output_file[MAX_NAME_SIZE];
@@ -14,6 +16,8 @@ typedef struct input_parameter_s{
 
 INPUT_PARAMETER new_input_parameter(void);
 
+
+//Struktur fuer Dateiinformationen (Name und anzahl der enthaltenen zeichen) + funktion zum erstellen mit defaultwerten
 typedef struct file_information{
 	char filename[MAX_NAME_SIZE];
 	int anzahl_zeichen;
@@ -21,12 +25,12 @@ typedef struct file_information{
 
 FILE_INFORMATION new_file_information(void);
 
-
+//Helferfunktionen
 void menue(void);
 void print_input_args(INPUT_PARAMETER args);
 void print_file_info(FILE_INFORMATION info);
 
-
+//Arbeiterfunktionen
 int check_input_parameter(char **parameter_list, INPUT_PARAMETER *parameter);
 FILE *load_file(char *filename, FILE_INFORMATION *file_info);
 FILE *create_new_file(char *filename);
@@ -38,6 +42,7 @@ int main(int argc, char *argv[]){
 		menue();
 	}
 	else{
+		
 		INPUT_PARAMETER input_parameter = new_input_parameter();
 		FILE_INFORMATION input_file_information = new_file_information();
 
@@ -45,19 +50,20 @@ int main(int argc, char *argv[]){
 		FILE *output_file;
 
 		if (FALSE == check_input_parameter(argv, &input_parameter)){
-			printf("Wert fuer den schluessel ungueltig (kein integer oder 0)!\n");
+			printf("Wert fuer Schluessel ungueltig (kein integer oder 0)!\n");
 			menue();
 		}
 		else if(NULL == (input_file = load_file(input_parameter.input_file, &input_file_information))){
-			printf("Konnte Eingabedatei nicht oeffnen (nicht vorhanden oder keine zugriffsrechte)!\n");
+			printf("Konnte Eingabedatei nicht oeffnen (nicht vorhanden oder keine Zugriffsrechte)!\n");
 			menue();
 		}
 		else if (NULL == (output_file = create_new_file(input_parameter.output_file))){
-			printf("Konnte Ausgabedatei nicht oeffnen (Datei bereits vorhanden oder Fehler aufgetreten)!\n");
+			printf("Konnte Ausgabedatei nicht oeffnen (Datei bereits vorhanden oder Fehler beim oeffnen aufgetreten)!\n");
 			menue();
 		}
 		else{
-			char buchstabe;
+			char buchstabe = 0;
+			//Datei zeichenweise einlesen und verschluesselt in Datei schreiben
 			for (int i = 0; i < input_file_information.anzahl_zeichen; i++){
 				buchstabe = getc(input_file);
 				buchstabe = verschluesseln(buchstabe, input_parameter.key);
@@ -68,6 +74,7 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
+//setzt Struktur auf Defaultwerte 
 INPUT_PARAMETER new_input_parameter(void){
 	INPUT_PARAMETER new_parameter;
 	memset(new_parameter.input_file, '\0', MAX_NAME_SIZE);
@@ -76,6 +83,7 @@ INPUT_PARAMETER new_input_parameter(void){
 	return new_parameter;
 }
 
+//setzt Struktur auf Defaultwerte
 FILE_INFORMATION new_file_information(void){
 	FILE_INFORMATION new_file_information;
 	memset(new_file_information.filename, '\0', MAX_NAME_SIZE);
@@ -103,6 +111,7 @@ void print_file_info(FILE_INFORMATION info){
 	return;
 }
 
+//laedt Datei und zaehlt die Zeichen
 FILE *load_file(char *filename, FILE_INFORMATION *file_info){
 	FILE *file;
 	if (NULL == (file = fopen(filename, "r"))){
@@ -117,6 +126,7 @@ FILE *load_file(char *filename, FILE_INFORMATION *file_info){
 	}
 }
 
+//erstellt neue Datei zum schreiben ,falls schon vorhanden rueckgabewert = NULL
 FILE *create_new_file(char *filename){
 	FILE *datei;
 	if (NULL !=  fopen(filename, "r")){
@@ -130,6 +140,7 @@ FILE *create_new_file(char *filename){
 	}
 }
 
+//ueberpruepft ob der eingegebene key ein gueltiges format hat auserdem werden die dateinamen auf MAX_NAME_SIZE - 1 zeichen begrenzt
 int check_input_parameter(char **parameter_list, INPUT_PARAMETER *parameter){
 	int status = 0;
 	int key = 0;
@@ -147,6 +158,7 @@ int check_input_parameter(char **parameter_list, INPUT_PARAMETER *parameter){
 		strncpy(parameter->output_file, parameter_list[3], MAX_NAME_SIZE - 1);
 		key = atoi(parameter_list[1]);
 
+		//falls key kleiner null wird er in ein benutzbares format umgewandelt (linksverschiebung wird zu rechtsverschiebung)
 		if (key < 0){
 			key = key * (-1);
 			key = key % 26;
@@ -162,6 +174,7 @@ int check_input_parameter(char **parameter_list, INPUT_PARAMETER *parameter){
 	return status;
 }
 
+//verschluesselt A...Z und a...z alle anderen zeichen werden nicht geaendert
 char verschluesseln(char character, int key){
 	if (character >= 'a' && character <= 'z'){
 		return ((character - 'a' + key) % 26) + 'a';
