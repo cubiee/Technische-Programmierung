deb9e5c99a269f9d329c6ede6dd7dc44dbefcc76
Joshua Harig <j.harig95@hotmail.de>
2018-06-01 12:26:04 +0200

    funktionsprototypen erstellt

--- Uebung10/klimadatenanalyse.c
+++ Uebung10/klimadatenanalyse.c
@@ -3,5 +3,27 @@
 #include "klimadatenanalyse.h"
 
 int leseKlimadaten(FILE* datei, KLIMADATEN k[], int anzahl_kopfzeilen, int max_anzahl, int* anzahl){
+	//hier sollen die klimadaten eingelesen werden
 	return 0;
+}
+
+void mache_temperatur(KLIMADATEN k[], TEMPERATUR t[], int anzahl){
+	//hier werden die klimadaten ausgewertet und in einem struct gespeichert
+	return;
+}
+
+void ausgabe_temperaturen(TEMPERATUR t[], int anzahl){
+	//hier werden die temperaturen ausgegeben;
+	return;
+}
+
+TEMPERATUR findeMaxTemperatur(TEMPERATUR t[], int anzahl){
+	TEMPERATUR max;
+	//hier soll die maximaltemperatur gesucht werden
+	return max;
+}
+
+void ausgabe_temperatur(TEMPERATUR t){
+	//hier wird eine temperatur ausgegeben
+	return;
 }
--- Uebung10/klimadatenanalyse.h
+++ Uebung10/klimadatenanalyse.h
@@ -18,7 +18,24 @@ typedef struct klimadaten_s{
 	float pm;
 } KLIMADATEN;
 
-int leseKlimadaten(FILE* datei, KLIMADATEN k[], int anzahl_kopfzeilen, int max_anzahl, int* anzahl);
+typedef struct temperatur_s{
+	int stat;
+	int jahr;
+	char monat[16];
+	int tag;
+	double tg;
+	double tn;
+	double tm;
+	double tx;
+} TEMPERATUR;
 
+int lese_Klimadaten(FILE* datei, KLIMADATEN k[], int anzahl_kopfzeilen, int max_anzahl, int* anzahl);
 
+void mache_temperatur(KLIMADATEN k[], TEMPERATUR t[], int anzahl);
+
+void ausgabe_temperaturen(TEMPERATUR t[], int anzahl);
+
+TEMPERATUR findeMaxTemperatur(TEMPERATUR t[], int anzahl);
+
+void ausgabe_temperatur(TEMPERATUR t);
 #endif
