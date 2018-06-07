bc83ebc957a5ca09bb6ca99575b0cccbd89d269b
jharig <jharig@kil-m-22.htw-saarland.de>
2018-06-07 12:51:11 +0200

    wertlesen hinzugefuegt

--- Uebung11/wertlesen.c
+++ Uebung11/wertlesen.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 #include "wertlesen.h"
+
+
+int wert_lesen(char *prompt, int min, int max, int *zahl){
+    int nmbr;
+    int status;
+    if (min > max){
+        int buffer = min;
+        min = max;
+        max = buffer;
+    }
+    if(min == max){
+        status = 0;
+    }
+    else{
+        printf(prompt);
+        int pruefe;
+        if(1 != (pruefe = scanf("%d", &nmbr))){
+            status = 0;
+        }else{
+            *zahl = nmbr;
+            status = 1;
+        }
+    }
+    return status;
+}
