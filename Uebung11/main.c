b13e837428d0d7cf4603c0d37012e55f9c8b8244
Joshua Harig <j.harig95@hotmail.de>
2018-06-09 12:06:32 +0200

    finale main datei mit auswahlstruktur

--- Uebung11/main.c
+++ Uebung11/main.c
@@ -20,7 +20,7 @@ int main(void){
 		case CMD_QUER:
 			//quersumme
 			while (!wert_lesen("Von welcher Zahl soll die quersumme gebildet werden? ", 0, 2147483647, &zahl)){
-				printf("Ungueltige eingabe!\nBitte erneut eingeben: ");
+				printf("Ungueltige eingabe!\n");
 			}
 			printf("Die Quersumme von %i ist %i\n",zahl, quersumme(zahl));
 			printf("\n");
@@ -28,7 +28,7 @@ int main(void){
 		case CMD_PYTH:
 			//pythagoras
 			while (!wert_lesen("Bis zu welcher zahl sollen die tripel ausgegeben werden? ", 0, 100000, &zahl)){
-				printf("Ungueltige eingabe!\nBitte erneut eingeben: ");
+				printf("Ungueltige eingabe!\n");
 			}
 			pythagoras(zahl);
 			printf("\n");
@@ -36,10 +36,10 @@ int main(void){
 		case CMD_TBAR:
 			//teilbar
 			while (!wert_lesen("Bitte Zahl eingeben: ", 0, 10000, &zahl)){
-				printf("Ungueltige eingabe!\nBitte erneut eingeben: ");
+				printf("Ungueltige eingabe!\n");
 			}
 			while (!wert_lesen("Bitte Teiler eingeben: ", 0, 10000, &teiler)){
-				printf("Ungueltige eingabe!\nBitte erneut eingeben: ");
+				printf("Ungueltige eingabe!\n");
 			}
 			if (teilbar(zahl, teiler)){
 				printf("%i / %i = %i\n", zahl, teiler, zahl / teiler);
