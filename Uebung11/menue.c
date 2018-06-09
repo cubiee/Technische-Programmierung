06332a7e754bc4cd64cdf2bb5ebcbd8440d57ff1
Joshua Harig <j.harig95@hotmail.de>
2018-06-09 02:27:36 +0200

    menuemodul hinzugefuegt

--- Uebung11/menue.c
+++ Uebung11/menue.c
@@ -1,3 +1,20 @@
-#include "menue.h"
+#include <stdio.h>
+#include <menue.h>
+#include <wertlesen.h>
 
-void menue(void);
+
+int menue(void){
+	char auswahl[] = {
+		"Bitte waehlen Sie :\n"
+		"  1 - Berechnung der Quersumme\n"
+		"  2 - Berechnung Pythagoras\n"
+		"  3 - Zahl n durch t teilbar\n"
+		"  0 - Ende\n"
+	};
+	printf(auswahl);
+	int wahl = 0;
+	while (!wert_lesen("", 0, 4, &wahl)){
+		printf("Ungueltige eingabe!\nBitte erneut eingeben :");
+	}
+	return wahl;
+}
